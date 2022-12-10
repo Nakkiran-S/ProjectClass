d1eaf7ceb4e81692573339e623ee3b855eda5bc6
Nakkiran-S <nakkiran93@mail.com>
2022-12-10 22:37:37 +0530

    First time push

---
+++ src/test/java/com/gitclasses/ClassA.java
@@ -0,0 +1,16 @@
+package com.gitclasses;
+
+public class ClassA {
+   private void method1() {
+	System.out.println("1");
+
+}
+   private void method2() {
+	System.out.println("2");
+
+}
+   private void method3() {
+	System.out.println("3");
+
+}
+}
